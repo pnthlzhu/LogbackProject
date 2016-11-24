33c9afe687a2f4f4bd8f175fa2bb8be0b43a86d8
hlzhu <zhu_huiling@psh.com.cn>
2016-11-24 16:19:29 +0800

    project init

---
+++ src/main/java/com/pnt/LogbackProject/App.java
@@ -0,0 +1,13 @@
+package com.pnt.LogbackProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
